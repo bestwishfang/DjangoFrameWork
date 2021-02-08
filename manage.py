693574552cc96442cd3b148bb909830d846d6480
ssfang <fss@originqc.com>
2021-02-08 11:24:24 +0800

    use gevent

--- manage.py
+++ manage.py
@@ -1,12 +1,29 @@
+# -*- coding: utf-8 -*-
+from gevent import monkey
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 
 from apps import create_app, models
 
+monkey.patch_all()
+
 app = create_app()
-manage = Manager(app)
+manager = Manager(app)
 migrate = Migrate(app, models)
-manage.add_command('db', MigrateCommand)
+manager.add_command('db', MigrateCommand)
+
+
+@manager.command
+def runserver_gevent():
+    from gevent import pywsgi
+    from werkzeug.debug import DebuggedApplication
+
+    dapp = DebuggedApplication(app, evalex=True)
+    ip_port = ('127.0.0.1', 5000)
+    server = pywsgi.WSGIServer(ip_port, dapp)
+    print("Server Running ...")
+    server.serve_forever()
+
 
 if __name__ == '__main__':
-    manage.run()
+    manager.run()
